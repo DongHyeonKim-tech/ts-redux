79753a7b3f5147128c4691401ec4753648ce0f09
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-01-03 14:00:55 +0900

    [추가] router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,18 @@ import "./App.css";
 import store from "app/store";
 import { Provider } from "react-redux";
 import IndexPage from "pages/index/IndexPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainSample from "features/sample/MainSample";
 
 function App() {
   return (
     <Provider store={store}>
-      <IndexPage />
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<IndexPage />} />
+          <Route path={"sample"} element={<MainSample />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   );
 }
