1c103eb4a8fab6795a7ceadecb464a000c7cc9ff
김동현 <71127040+DongHyeonKim-tech@users.noreply.github.com>
2024-01-03 14:00:25 +0900

    [추가] 샘플 페이지 추가

---
+++ src/features/sample/MainSample.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "app/store";
+
+const MainSample = () => {
+  const arrDatabaseList = useSelector(
+    (state: RootState) => state.indexReducer.arrDatabaseList
+  );
+
+  const onCheckDatabaseList = () =>
+    console.log("arrDatabaseList: ", arrDatabaseList);
+
+  useEffect(
+    () => console.log("samplePage: ", arrDatabaseList),
+    [arrDatabaseList]
+  );
+  return (
+    <div>
+      <p>MainSample</p>
+      <button onClick={() => onCheckDatabaseList()}>check database list</button>
+    </div>
+  );
+};
+
+export default MainSample;
---
+++ src/pages/sample/SamplePage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MainSample from "features/sample/MainSample";
+
+const SamplePage = () => {
+  return <MainSample />;
+};
+
+export default SamplePage;
